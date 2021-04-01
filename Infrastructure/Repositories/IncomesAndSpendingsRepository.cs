93a4f209ee08ee6a1a5599e4add8bc54ae9fc23d
Braian Baldino <baldino.bab@hotmail.com>
2021-04-01 20:40:54 -0300

    remove using

--- Infrastructure/Repositories/IncomesAndSpendingsRepository.cs
+++ Infrastructure/Repositories/IncomesAndSpendingsRepository.cs
@@ -7,7 +7,6 @@ using Model.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Repositories
